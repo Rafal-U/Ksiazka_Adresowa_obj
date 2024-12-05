40531daa4bd0e995bf0fab32006fa6683c306894
Rafal-U <rafal.urawski.programista@gmail.com>
2024-12-05 22:34:35 +0100

    Divide KsiazkaAdresowa to smaller class

---
+++ UzytkownikMenedzer.cpp
@@ -0,0 +1,64 @@
+#include "UzytkownikMenedzer.h"
+
+int UzytkownikMenedzer::pobierzIdNowegoUzytkownika()
+{
+    if (uzytkownicy.empty() == true)
+        return 1;
+    else
+        return uzytkownicy.back().pobierzId() + 1;
+}
+
+bool UzytkownikMenedzer::czyIstniejeLogin(string login)
+{
+    for (int i = 0; i < uzytkownicy.size(); i++)
+    {
+        if (uzytkownicy[i].pobierzLogin() == login)
+        {
+            cout << "Istnieje uzytkownik o takim loginie" << endl;
+            return true;
+        }
+    }
+    return false;
+}
+
+void UzytkownikMenedzer::rejestracjaUzytkownika()
+{
+    Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
+
+    uzytkownicy.push_back(uzytkownik);
+    plikZUzytkownikami.dopiszUzytkownikaDoPliku(uzytkownik);
+
+    cout << endl << "Konto zalozono pomyslnie" << endl << endl;
+    system("pause");
+}
+
+Uzytkownik UzytkownikMenedzer::podajDaneNowegoUzytkownika()
+{
+    Uzytkownik uzytkownik;
+    string login, haslo;
+    uzytkownik.ustawId(pobierzIdNowegoUzytkownika());
+
+    do
+    {
+        cout << "Podaj login: " << endl;
+        cin >> login;
+        uzytkownik.ustawLogin(login);
+    }
+    while (czyIstniejeLogin(uzytkownik.pobierzLogin()) == true);
+
+    cout << "Podaj haslo: " << endl;
+    cin >> haslo;
+    uzytkownik.ustawHaslo(haslo);
+
+    return uzytkownik;
+}
+
+void UzytkownikMenedzer::wypiszWszystkichUzytkownikow()
+{
+    for (int i = 0; i < uzytkownicy.size(); i++)
+    {
+        cout << uzytkownicy[i].pobierzId() << endl;
+        cout << uzytkownicy[i].pobierzLogin() << endl;
+        cout << uzytkownicy[i].pobierzHaslo() << endl;
+    }
+}
