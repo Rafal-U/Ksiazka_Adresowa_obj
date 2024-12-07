04254ec7803f45faa7eb7178c37b206f162d231b
Rafal-U <rafal.urawski.programista@gmail.com>
2024-12-07 20:58:11 +0100

    Added setup filename at begining

--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -9,7 +9,7 @@ class KsiazkaAdresowa
     UzytkownikMenedzer uzytkownikMenedzer;
 
 public:
-    KsiazkaAdresowa();
+    KsiazkaAdresowa(string nazwaPlikuZUzytkownikami) : uzytkownikMenedzer(nazwaPlikuZUzytkownikami){uzytkownikMenedzer.wczytajUzytkownikowZPliku();};
     void rejestracjaUzytkownika();
     void wypiszWszystkichUzytkownikow();
 
